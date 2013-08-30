a1315338e13a59743eae5e4baa217b2470e94bce
Guillermo López Leal <willyaranda@gmail.com>
2013-08-30 10:47:21 +0200

    Ping interval for Push (19 minutes)

--- partner-prefs.js
+++ partner-prefs.js
@@ -1,2 +1,6 @@
 pref("services.push.serverURL", "wss://ua.push.tefdigital.com");
+// LATAM carriers: 20 minutes minimum.
+// Since we only have one variant for all countries, this is the best value
+// for the worst country: 19 minutes
+pref("services.push.pingInterval", "1140000"); 
 pref("network.proxy.no_proxies_on", "localhost, 127.0.0.1, *.push.tefdigital.com");
